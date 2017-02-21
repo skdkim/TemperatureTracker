e3c948418e8d8aec17afc16834641787b5650e68
Sung Kwang David Kim <sungkwangkim@gmail.com>
2017-02-20 17:22:24 -0800

    create tests for all methods

--- TemperatureTracker.js
+++ TemperatureTracker.js
@@ -1,10 +1,86 @@
 class TemperatureTracker{
-  constructor(num = 35){
-    this.temp = num;
+  constructor(){
+    this.maxTemp;
+    this.minTemp;
+    this.allTemps = {};
+    this.tempSums;
+    this.totalTemps;
+    this.modeTemp;
+    this.modeTempOccurance;
   }
 
-  getTemp() {
-    return this.temp;
+  getMax(){
+    if (!this.maxTemp){
+      throw new Error ('no temperatures added');
+    }
+
+    return this.maxTemp;
+  }
+
+  getMin(){
+    if (!this.maxTemp){
+      throw new Error ('no temperatures added');
+    }
+
+    return this.minTemp;
+  }
+
+  getMean(){
+    if (!this.maxTemp){
+      throw new Error ('no temperatures added');
+    }
+
+    return Math.round((this.tempSums / this.totalTemps) * 100) / 100;
+  }
+
+  getMode(){
+    if (!this.maxTemp){
+      throw new Error ('no temperatures added');
+    }
+    
+    return this.modeTemp;
+  }
+
+  insert(newTemp){
+    // checks for valid entry
+    if (typeof(newTemp) !== 'number'){
+      throw new Error ('invalid entry');
+    }
+    // taking care of mean
+    if (!this.tempSums){
+      this.tempSums = newTemp;
+      this.totalTemps = 1;
+    } else {
+      this.tempSums += newTemp;
+      this.totalTemps += 1;
+    }
+
+    // taking care of min and max
+    if (!this.maxTemp){
+      this.maxTemp = newTemp;
+      this.minTemp = newTemp;
+    }
+    if (newTemp > this.maxTemp){
+      this.maxTemp = newTemp;
+    }
+    if (newTemp < this.minTemp){
+      this.minTemp = newTemp;
+    }
+
+    // taking care of insert and mode
+    if (!this.allTemps[newTemp]){
+      this.allTemps[newTemp] = 1;
+      if (!this.modeTemp){
+        this.modeTemp = newTemp;
+        this.modeTempOccurance = 1;
+      }
+    } else {
+      this.allTemps[newTemp] += 1;
+      if (this.modeTempOccurance < this.allTemps[newTemp]){
+        this.modeTemp = newTemp;
+        this.modeTempOccurance = this.allTemps[newTemp];
+      }
+    }
   }
 }
 
--- test/tempTrackerTest.js
+++ test/tempTrackerTest.js
@@ -2,11 +2,116 @@ let assert = require('assert');
 
 let TemperatureTracker = require('../TemperatureTracker.js');
 
-describe('#getTemp()', () => {
+describe('#insert()', () => {
   let temp = new TemperatureTracker();
 
-  it('should work', () => {
-    assert.equal(temp.getTemp(), 35);
+  it('should throw and error if value is not a number', () => {
+    assert.throws(() => {
+      temp.insert("hello");
+    }, 'invalid entry');
   });
 
+  it('should insert properly the first temperature', () =>{
+    temp.insert(5);
+
+    assert.equal(temp.allTemps[5], true);
+  });
+
+  it('should insert properly consecutive temperatures', () => {
+    temp.insert(20);
+
+    assert.equal(temp.allTemps[20], true);
+  });
+
+  it('should realize when the same temperture has been added', () => {
+    temp.insert(20);
+
+    assert.equal(temp.allTemps[20], 2);
+  });
+});
+
+describe('#getMax()', () => {
+  let temp = new TemperatureTracker();
+  it('should throw and error if no temperatures are added', () => {
+    assert.throws(() => {
+      temp.getMax();
+    }, 'no temperatures added');
+  });
+
+  it('should return the highest temperature', () => {
+    temp.insert(5);
+
+    assert.equal(temp.getMax(), 5);
+  });
+
+  it('should update highest temperature', () => {
+    temp.insert(55);
+    temp.insert(-100);
+
+    assert.equal(temp.getMax(), 55);
+  });
+});
+
+describe('#getMin()', () => {
+  let temp = new TemperatureTracker();
+  it('should throw and error if no temperatures are added', () => {
+    assert.throws(() => {
+      temp.getMin();
+    }, 'no temperatures added');
+  });
+
+  it('should return the lowest temperature', () => {
+    temp.insert(5);
+
+    assert.equal(temp.getMin(), 5);
+  });
+
+  it('should update lowest temperature', () => {
+    temp.insert(55);
+    temp.insert(-100);
+
+    assert.equal(temp.getMin(), -100);
+  });
+});
+
+describe('#getMean()', () => {
+  let temp = new TemperatureTracker();
+  it('should throw and error if no temperatures are added', () => {
+    assert.throws(() => {
+      temp.getMean();
+    }, 'no temperatures added');
+  });
+
+  it('should return the mean of all temperatures with 2 decimals', () => {
+    temp.insert(10);
+    temp.insert(20);
+    temp.insert(19);
+
+    assert.equal(temp.getMean(), 16.33);
+  });
+});
+
+describe('#getMode()', () => {
+  let temp = new TemperatureTracker();
+  it('should throw and error if no temperatures are added', () => {
+    assert.throws(() => {
+      temp.getMode();
+    }, 'no temperatures added');
+  });
+
+  it('should return the most often occuring temperature', () => {
+    temp.insert(5);
+    temp.insert(10);
+    temp.insert(5);
+
+    assert.equal(temp.getMode(), 5);
+  });
+
+  it('should properly update mode', () => {
+    temp.insert(3);
+    temp.insert(3);
+    temp.insert(3);
+
+    assert.equal(temp.getMode(), 3);
+  });
 });
